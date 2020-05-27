165426740fbdb2a75c8c6a19fc22036696214fc6
Benitez-Juarez-Jorge <jorgebenitezjuarez15@gmail.com>
2020-05-27 12:56:18 -0500

    ejercicos del 1 al 4
    Benitez Juarez Jorge

---
+++ README.py
@@ -0,0 +1,4 @@
+print("1.-supe como realizar el programa ya que solo eran darles valor a una letra y sumarla por la otra letra, con el valor deseado")
+print("2.- aqui tuve que investigar un poco, pero era igual un poco faci, se trataba se sumar los numeros consecutivos entre si.")
+print("3.-se me hizo algo complicado, haci que tuve que investigar y supe que los numeros feos son factores primos e hice el programa.")
+print("4.-no comprendi muy bien, me ayudaron y haci pude realizar el programa, aun tengo dudas, pero medio que le comprendi un poco.")
